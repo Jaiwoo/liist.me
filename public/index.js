15a1ab0b11d85620a14028b0340c0a26c307a8bd
Jaiwoo <info@jaiwoo.me>
2018-09-05 23:07:20 -0400

    implemented STATE object and rendering functions, all functions working

--- public/index.js
+++ public/index.js
@@ -1,51 +1,86 @@
-//
-// ─── JQUERY SELECTORS─────────────────────────────────────────────────────────────
-//
 
-//TABLE ELEMENTS
-const liistsTableBody = $('#liists-table-body');
-const liistTableBody = $('#liist-table-body');
-const liistName = $('#liist-name');
-const liistDescription = $('#liist-description');
-const liistOwner = $('#liist-owner');
-
-// BUTTONS
-const getLiistsBtn = $('#get-liists-btn');
-const homeButton = $('#banner-text');
-const addSongButton = $('#add-song-btn');
-const createLiistButton = $('#create-btn');
-
-// PAGES
-const landingPage = $('#landing-container');
-const liistsPage = $('#liists-container');
-const liistPage = $('#liist-container');
-const createLiistPage = $('#create-liist-container');
-const addSongPage = $('#add-song-container');
+const STATE = {};
 
 //
 // ─── API CALLS ──────────────────────────────────────────────────────────────────
 //
 
-function getRecentLiists (callback) {
-  setTimeout(function(){ callback(mockDatabase);}, 100);
+// GET /liists
+function getRecentLiists(callback) {
+  setTimeout(function() {
+    callback(mockDatabase);
+  }, 100);
 }
 
-
-function getLiistByID (ID, callback) {
+// GET to /liists:ID
+function getLiistByID(ID) {
   const liist = mockDatabase.liists.find(function(element) {
     return element.id == ID;
   });
-  callback(liist);
+  
+  setState({
+    liistID: ID,
+    liist: liist,
+    page: 'liist'
+  });
+}
+
+// PUT to /liists:ID
+function addSongToLiist(ID, userInput) {
+
+  const songToAdd = {
+    title: `${userInput.title}`,
+    artist: `${userInput.artist}`,
+    addedBy: `${userInput.addedBy}`,
+    addedDate: new Date(),
+    stars: 0
+  };
+
+  const liistIndex = mockDatabase.liists.findIndex(function(element) {
+    return element.id == ID;
+  });
+
+  mockDatabase.liists[liistIndex].songs.push(songToAdd);
+
+
+  return Promise.resolve();
+}
+
+// POST to /create/liist
+function createNewLiist (userInput) {
+  // POST API call to /create/liist
+  let newLiist = {
+    id: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
+    songs: []
+  };
+  Object.assign(newLiist, userInput);
+  mockDatabase.liists.push(newLiist);
+
+  return new Promise;
+}
+
+// DELETE to /liist:ID
+function deleteLiistByID (ID) {
+  mockDatabase.liists.splice(ID, 1);
+  getRecentLiists(data => {
+    setState({ liists: data, page: 'liists' });
+  });
 }
 
 
+
+
+
+
 //
-// ─── RECENT LIISTS CONTROLS ─────────────────────────────────────────────────────
+// ─── RECENT LIISTS LOGIC ───────────────────────────────────────────────────────
 //
 
-function displayLiists (data) {
+function generateLiistsTable(data) {
+
+  let liistsTableRows = [];
   for (let index in data.liists) {
-    liistsTableBody.append(
+    liistsTableRows.push(
       `<tr id="${data.liists[index].id}" class="liist-table-row">
         <td>${data.liists[index].name}</td>
         <td>${data.liists[index].owner}</td>
@@ -54,88 +89,224 @@ function displayLiists (data) {
       </tr>`
     );
   }
-  // add row click handlers
-  $('#liists-table .liist-table-row').click(function() {
-    liistsPage.hide();
-    liistsTableBody.empty();
-    liistPage.show();
-    clearLiistTable();
-    getAndDisplayLiist($(this).attr('id'));
-  });
+
+  liistsTableRows = liistsTableRows.join('');
+
+  const liistsTable = `
+    <div id="liists-container" class="container">
+      <h2 id="liists-container-title" class="container-title">Recent Liists</h2>
+      <table id="liists-table">
+        <thead>
+          <tr>
+            <th>liist name</th>
+            <th>created by</th>
+            <th style="width: 10%">🎵's</th>
+            <th style="width: 20%">updated</th>
+          </tr>
+        </thead>
+        <tbody id="liists-table-body">
+          ${liistsTableRows}
+        </tbody>
+      </table>
+    </div>`;
+
+  return liistsTable;
 }
 
-function getAndDisplayRecentLiists () {
-  getRecentLiists(displayLiists);
+
+//
+// ─── STATE & RENDER LOGIC ───────────────────────────────────────────────────────
+//
+
+function setState(newState) {
+  Object.assign(STATE, newState);
+  render(STATE);
+}
+
+function render(state) {
+
+  // render liists page
+  if (state.page === 'liists') {
+    const liistsTableHtml = generateLiistsTable(state.liists);
+    $('#data').html(liistsTableHtml);
+    $('#liists-table .liist-table-row').click(function() {
+      getLiistByID($(this).attr('id'));
+    });
+
+    // render liist page
+  } else if (state.page === 'liist') {
+    const liistTableHtml = generateLiistTable(state.liist);
+    $('#data').html(liistTableHtml);
+    // add-song click handler
+    $('#add-song-btn').on('click', function() {
+      const addSongFormHTML = generateAddSongForm(state.liist);
+      $('#data').html(addSongFormHTML);
+      handleAddSongSubmit(STATE.liistID);
+    });
+    // delete liist click handler
+    $('#delete-liist-btn').on('click', function() {
+      let del = confirm('Are you sure you want to delete this liist?');
+      if (del) {
+        deleteLiistByID(state.liistID);
+      }
+    });
+  }
+  // render create-liist page
+  else if (state.page === 'create-liist') {
+    const createFormHTML = generateCreateForm();
+    $('#data').html(createFormHTML);
+    handleCreateLiistSubmit();
+  }
 }
 
 //
-// ─── LIIST CONTROLS ─────────────────────────────────────────────────────────────
+// ─── LIIST LOGIC ────────────────────────────────────────────────────────────────
 //
 
-function displayLiist (liist) {
-  // add song button handler
-  addSongButton.on('click', function() {
-    $('#add-song-liistName').text(`${liist.name}`);
-    liistPage.hide();
-    addSongPage.show();
-  });
-
-  liistName.text(`${liist.name}`);
-  liistOwner.text(`By: ${liist.owner}`);
-  liistDescription.text(`${liist.description}`);
+function generateLiistTable (liist) {
+  let liistTableRows = [];
   for (let index in liist.songs) {
-    liistTableBody.append(
+    liistTableRows.push(
       `<tr class="liist-table-row">
         <td>${liist.songs[index].title}</td>
         <td>${liist.songs[index].artist}</td>
         <td>${liist.songs[index].addedBy}</td>
-        <td>${liist.songs[index].stars}</td>`
+        <td>${liist.songs[index].stars}</td>
+      </tr>`
     );
   }
+
+  liistTableRows = liistTableRows.join('');
+
+  const liistTable = `
+    <div id="liist-container" class="container">
+      <div id="liist-container-info" class="container-info">
+        <h2 id="liist-name">${liist.name}</h2>
+        <h3 id="liist-owner">Created By: ${liist.owner}</h3>
+        <div id="liist-description">${liist.description}</div>
+      </div>
+      <button id="add-song-btn" class="liist-button">Add Song</button>
+      <button id="delete-liist-btn" class="liist-button">Delete Liist</button>
+      <table id="liist-table">
+        <thead>
+          <tr>
+            <th style="width: 40%">track</th>
+            <th style="width: 25%">artist</th>
+            <th style="width: 25%">added by</th>
+            <th style="width: 10%">❤︎'s</th>
+          </tr>
+        </thead>
+        <tbody id="liist-table-body">
+          ${liistTableRows}
+        </tbody>
+      </table>
+    </div>`;
+
+  return liistTable;
 }
 
-function getAndDisplayLiist (ID) {
-  getLiistByID(ID, displayLiist);
+//
+// ─── ADD SONG LOGIC ─────────────────────────────────────────────────────────────
+//
+
+function generateAddSongForm (liist) {
+  const addSongFormHTML = `
+    <div id="add-song-container" class="container">
+      <form id="add-song-form" role="form" action="#">
+        <legend>Add a song to ${liist.name}.</legend>
+        <label for="add-song-addedBy">Your Name:</label>
+        <br>
+        <input id="add-song-addedBy" type="text">
+        <br>
+        <label for="add-song-name">Song Name:</label>
+        <br>
+        <input id="add-song-name" type="text">
+        <br>
+        <label for="add-song-artist">Artist:</label>
+        <br>
+        <input id="add-song-artist" type="text">
+        <br>
+        <input id="add-song-submit" class="form-submit" type="submit" value="Add Song">
+      </form>
+    </div>
+  `;
+
+  return addSongFormHTML;
 }
 
-function clearLiistTable () {
-  liistName.empty();
-  liistOwner.empty();
-  liistDescription.empty();
-  liistTableBody.empty();
+function handleAddSongSubmit(ID) {
+  $('#add-song-form').on('submit', function(e) {
+    e.preventDefault();
+
+    const userInput = {
+      title: $('#add-song-name').val(),
+      artist: $('#add-song-artist').val(),
+      addedBy: $('#add-song-addedBy').val()
+    };
+
+    addSongToLiist(ID, userInput).then(getLiistByID(ID));
+  });
 }
 
+//
+// ─── CREATE LIIST LOGIC ─────────────────────────────────────────────────────────
+//
+
+function generateCreateForm () {
+  const createFormHTML = `
+    <div id="create-liist-container" class="container">
+      <form id="create-liist-form" role="form" action="#">
+        <legend>Create a new liist & give it a fun description to share with others.</legend>
+        <label for="create-owner-name">Your Name:</label>
+        <br>
+        <input id="create-owner-name" type="text">
+        <br>
+        <label for="create-liist-name">Liist Name:</label>
+        <br>
+        <input id="create-liist-name" type="text">
+        <br>
+        <label for="create-liist-description">Liist Description:</label>
+        <br>
+        <textarea id="create-liist-description" type="text"></textarea>
+        <br>
+        <input id="create-liist-submit" class="form-submit" value="Create Liist" type="submit">
+      </form>
+    </div>  
+  `;
+
+  return createFormHTML;
+}
+
+function handleCreateLiistSubmit () {
+  $('#create-liist-form').submit(function(e) {
+    e.preventDefault();
+    const userInput = {
+      owner: $('#create-owner-name').val(),
+      name: $('#create-liist-name').val(),
+      description: $('#create-liist-description').val()
+    };
+
+    createNewLiist(userInput).then(function(ID) {
+      getLiistByID(ID);
+    });
+  });
+}
 
 
 //
-// ─── BUTTON HANDLERS─────────────────────────────────────────────────────────────
+// ─── NAV-BUTTON HANDLERS──────────────────────────────────────────────────────────
 //
 
-getLiistsBtn.on('click', function() {
-  liistsTableBody.empty();
-  getAndDisplayRecentLiists();
-  createLiistButton.show();
-  landingPage.hide();
-  liistPage.hide();
-  createLiistPage.hide();
-  addSongPage.hide();
-  liistsPage.show();
+$('#get-liists-btn').on('click', function() {
+  getRecentLiists(data => {
+    setState({ liists: data, page: 'liists' });
+  });
 });
 
-createLiistButton.on('click', function() {
-  landingPage.hide();
-  createLiistButton.hide();
-  liistPage.hide();
-  liistsPage.hide();
-  addSongPage.hide();
-  createLiistPage.show();
-
+$('#create-btn').on('click', function() {
+  setState({ page: 'create-liist'});
 });
 
-homeButton.on('click', function() {
-  liistsPage.hide();
-  liistsTableBody.empty();
-  liistPage.hide();
-  clearLiistTable();
-  landingPage.show();
-});
+// homeButton.on('click', function() {
+
+// });
